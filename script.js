969d3b0034a13f752be8d4cf844e992af94020aa
mahmoud fouad <mfapolice@gmail.com>
2022-05-20 16:32:31 +0200

    create colors

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hi there');
+'use strict';
+
+//DOM variables
+const container = document.querySelector('.container');
+
+// colors array
+let colorArr = [];
+let favouriteColors = [];
+
+// creating a new elements
+const newDiv = divNums => {
+  for (let i = 0; i < divNums; i++) {
+    const div = document.createElement('div');
+    div.classList.add('color-container');
+    container.insertAdjacentElement('afterbegin', div);
+  }
+};
+newDiv(130);
+
+// Generate random Color
+const color = function () {
+  let colorCode = '';
+  let hex = '0123456789abcdef';
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hex.length);
+    colorCode += hex.substring(random, random + 1);
+  }
+  return `#${colorCode}`;
+};
+color();
+
+function displayUI() {
+  const all = document.querySelectorAll('.color-container');
+  all.forEach(ele => {
+    [ele.textContent, ele.style.backgroundColor] = [color(), color()];
+  });
+}
+displayUI();
